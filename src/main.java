1aa78aa3349b711459ed7c330e5cd79bd315d326
omirfarhan <162728245+omirfarhan@users.noreply.github.com>
2025-07-29 10:21:47 +0600

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class main {
+
+
+
+
+    public static void main(String[] args){
+
+        int[] nums=new int[]{4,-1};
+
+        int small=Integer.MAX_VALUE;
+        if (nums[0]<small){
+            small=nums[0];
+            System.out.println(small);
+        }
+
+        /*
+        for (int i=0;i<nums.length;i++){
+
+
+
+           //
+        }
+
+         */
+
+
+
+    }
+
+
+
+}
