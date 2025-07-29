bb70faeaa996d46e3fc191f43426a24f7182dcc0
omirfarhan <162728245+omirfarhan@users.noreply.github.com>
2025-07-29 11:52:03 +0600

    git commit

--- src/main.java
+++ src/main.java
@@ -7,23 +7,30 @@ public class main {
 
     public static void main(String[] args){
 
-        int[] nums=new int[]{4,-1};
+        int[] nums=new int[]{4,-1,44,36,22,483,284};
 
-        int small=Integer.MAX_VALUE;
-        if (nums[0]<small){
-            small=nums[0];
-            System.out.println(small);
-        }
+        int minvalue=Integer.MAX_VALUE;
+        int large=Integer.MIN_VALUE;
 
-        /*
+        int[] largeindex=new int[1];
+        int[] smallerindex=new int[1];
+
         for (int i=0;i<nums.length;i++){
 
+            if (large<nums[i]){
+                large=nums[i];
+                largeindex=new int[i];
 
+            } else if (nums[i]<minvalue) {
+               minvalue=nums[i];
+               smallerindex=new int[i];
+            }
 
-           //
         }
+        System.out.println("The small number is: "+minvalue+" & indexnumber: "+smallerindex.length);
+        System.out.println("\n");
+        System.out.println("The large number is: "+large+" & indexnumber: "+largeindex.length);
 
-         */
 
 
 
